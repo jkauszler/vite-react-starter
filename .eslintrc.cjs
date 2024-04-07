830691e55487c602412c0ca5597f4b544c2e0c9a
Justin Kauszler <jkauszler@gmail.com>
2024-04-06 20:03:51 -0400

    build: added import linters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,33 @@ module.exports = {
 	env: { browser: true, es2020: true },
 	extends: [
 		"xo",
-		"xo-react"
+		"xo-react",
+		"plugin:import/recommended"
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		project: './tsconfig.json',
+		project: "tsconfig.json",
+		tsconfigRootDir: __dirname,
+		sourceType: "module",
 	},
 	plugins: [],
 	rules: {
 		'object-curly-spacing': ["error", "always"],
 		'react/react-in-jsx-scope': 'off',
 		'no-shadow': 2,
+		'capitalized-comments': 'off',
 	},
-	settings: {},
+	settings: {
+		"import/resolver": {
+			node: {
+				extensions: [
+					".js",
+					".jsx",
+					".ts",
+					".tsx"
+				]
+			}
+		}
+	}
 }
